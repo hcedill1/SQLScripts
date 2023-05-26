fea0bc1779c07d9df4f22475300ee5dbbef275f1
hernancedillo <hcedill1@asu.edu>
2023-05-26 14:24:04 -0500

    revised salary

--- InsertData.sql
+++ InsertData.sql
@@ -8,3 +8,15 @@ INSERT INTO EmployeeDemographics VALUES
 (1007, 'Meredith','Palmer',32,'Female'),
 (1008, 'Stanley','Hudson',38,'Male'),
 (1009, 'Kevin','Malone',31,'Male');
+
+INSERT INTO EmployeeSalary VALUES 
+(1001,'Salesman',45000),
+(1002,'Receptionist',36000),
+(1003,'Salesman',63000),
+(1004,'Accountant',47000),
+(1005,'HR',50000),
+(1006,'Regional Manager',65000),
+(1007,'Supplier Relations',41000),
+(1008,'Salesman',48000),
+(1009,'Accountant',42000);
+
